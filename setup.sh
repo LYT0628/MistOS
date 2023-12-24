ca7c59bf170d295d711ca0ace4fe2516ee937ef6
LYT0628 <lyt.0628@qq.com>
2023-12-24 22:47:52 +0800

    update workflow

--- setup.sh
+++ setup.sh
@@ -2,14 +2,13 @@
 apt-get update  
 
 apt install git \ 
-    nasm \ 
-    build-essential \
-    xorg-dev  \
-    libgtk2.0-dev \
-    vgabios bximage  
+nasm \ 
+build-essential  xorg-dev  \
+libgtk2.0-dev vgabios bximage  
 
 
 git clone https://github.com/LYT0628/bochs-2.4.5.git \
     && cd bochs-2.4.5 \
     && ./configure --enable-debugger --enable-disasm  \
     && make && sudo make install 
+rm -rf ./bochs-2.4.5
