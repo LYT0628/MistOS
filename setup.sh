0ef392867fc6ffc9d0bdeb3bc8ddab6d567b4f8c
LYT0628 <lyt.0628@qq.com>
2023-12-24 22:45:58 +0800

    retry

--- setup.sh
+++ setup.sh
@@ -2,9 +2,11 @@
 apt-get update  
 
 apt install git \ 
-nasm \ 
-build-essential  xorg-dev  \
-libgtk2.0-dev vgabios bximage  
+    nasm \ 
+    build-essential \
+    xorg-dev  \
+    libgtk2.0-dev \
+    vgabios bximage  
 
 
 git clone https://github.com/LYT0628/bochs-2.4.5.git \
