3722dd56304b4a1de618524a8334890a6e7840b8
LYT0628 <lyt.0628@qq.com>
2023-12-29 23:46:04 +0800

    start paging

--- boot/boot16.asm
+++ boot/boot16.asm
@@ -1,10 +1,7 @@
 
 %include	"pm.inc"	; 常量, 宏
+%include 	"boot.inc"
 
-BOOT32_LBA  						EQU 0x2
-BOOT32_LOGIC_ADDR   		EQU 0x1000
-BOOT32_PHYSICAL_ADDR   	EQU 0x10000  
-BOOT32_LIMIT 						EQU 0xFFFFF
 
 
 [SECTION .boot16 vstart=0x7c00]
@@ -16,11 +13,11 @@ LABEL_BEGIN:
 	mov	ss, ax
 
 
-	; 加载 GDTR
-	lgdt	[GdtPtr ]
 
-	; 关中断
-	cli
+	lgdt	[GdtPtr]	; 加载 GDTR
+
+
+	cli	; 关中断
 
 	; 打开地址线A20
 	in	al, 92h
@@ -29,8 +26,7 @@ LABEL_BEGIN:
 
 
 
-;;;软盘驱动;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;软盘驱动,读取boot32内核代码;;;;;;;;;;;;;;;;;;;;;
 	mov ax, BOOT32_LOGIC_ADDR
 	mov es, ax 
 
@@ -38,51 +34,44 @@ LABEL_BEGIN:
 	mov dh, 0 ; 0磁头
 	mov cl, 2 ; 扇区2
 
-
 	mov ah, 0x02 ; 读盘操作
 	mov al, 1 ;一个扇区
 	mov bx, 0
 	mov dl, 0x00 ;A驱动器
 	int 0x13 ; bios中断
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;boot32读取结束;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	; 准备切换到保护模式
-	; 注意，跳到保护模式后不能 mov 段寄存器了
+
+
+
+; 注意，跳到保护模式后不能 mov cs段寄存器了
 	mov	eax, cr0
 	or	eax, 1
 	mov	cr0, eax
-	; 真正进入保护模式
-	; jmp	dword SelectorCode32:0	; 执行这一句会把 SelectorCode32 装入 cs,
+; 真正进入保护模式
 	jmp	dword SelectorCode32: BOOT32_PHYSICAL_ADDR
-					; 并跳转到 Code32Selector:0  处
+; END of [SECTION .boot16]
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
-; END of read_hard_disk_0
-; END of [SECTION .s16]
-
-; [SECTION .gdt]
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; GDT
 ;                              段基址,       段界限     , 属性
-LABEL_GDT:	   Descriptor       0,                0, 0           ; 空描述符
-LABEL_DESC_CODE32: Descriptor   0, 				BOOT32_LIMIT, DA_C + DA_32; 非一致代码段
-LABEL_DESC_DATA: Descriptor     0,     		BOOT32_LIMIT,    DA_DRW + DA_32
+LABEL_GDT:	   Descriptor       0,                	 0, 						0           	 ; 空描述符
+LABEL_DESC_CODE32: Descriptor   0, 				BOOT32_LIMIT, 						DA_C + DA_32	 ; 内核代码段
+LABEL_DESC_DATA: Descriptor     0,     		BOOT32_LIMIT,   	 				DA_DRW + DA_32 ; 内核数据段
 ; GDT 结束
 
-GdtLen		equ	$ - LABEL_GDT	; GDT长度
-GdtPtr		dw	GdtLen - 1	; GDT界限
-		      dd	LABEL_GDT		; GDT基地址
+GdtLen		EQU	$ - LABEL_GDT	; GDT长度
+GdtPtr		DW	GdtLen - 1	; GDT界限
+		      DD	LABEL_GDT		; GDT基地址
 
 ; GDT 选择子
-SelectorCode32		equ	LABEL_DESC_CODE32	- LABEL_GDT
+SelectorCode32		EQU	LABEL_DESC_CODE32	- LABEL_GDT
 SelectorData      EQU LABEL_DESC_DATA - LABEL_GDT
 ; END of [SECTION .gdt]
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 times 510 - ($ - $$) DB 0
--- boot/boot32.asm
+++ boot/boot32.asm
@@ -1,2 +1,42 @@
-  org 0x10000
+;; 进入这里就不需要考虑启动盘末尾标识的问题了，可以使用多个section。
+%include	"pm.inc"
+%include 	"boot.inc"
+
+
+[SECTION .boot32 vstart=0x10000]
+[BITS 32]
+  ; 为64 位模式准备e新的 段描述符
+  mov ax, SelectorData
+  mov ds, ax 
+
+  lgdt [GdtPtr]
+
   jmp $
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; GDT 需要重新设置， 64位模式下段描述符只有 L, P, DPL, S, TYPE 有作用
+;                              段基址,       段界限     , 属性
+LABEL_GDT:	   Descriptor       0,                	 0, 						0           	 ; 空描述符
+LABEL_DESC_CODE32: Descriptor   0, 				           0, 						DA_L + DA_CR ; 内核代码段，可读，长模式，存在，特权级0
+LABEL_DESC_DATA: Descriptor     0,     		           0,   	 				DA_DRW  ; 内核数据段
+; GDT 结束
+
+GdtLen		equ	$ - LABEL_GDT	; GDT长度
+GdtPtr		dw	GdtLen - 1	; GDT界限
+		      dd	LABEL_GDT		; GDT基地址
+
+; GDT 选择子
+SelectorCode32		equ	LABEL_DESC_CODE32	- LABEL_GDT
+SelectorData      EQU LABEL_DESC_DATA - LABEL_GDT
+; END of [SECTION .gdt]
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+; 为boot32 留下 64KB 空间
+org 0x10000
+  times 511 * 8 DB 0x0 ; 511个，64位(8个byte) 的 0
+  DQ 0x1
